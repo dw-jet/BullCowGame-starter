5d6bf28701955195905c4460f412d352f85b412c
James Teitsworth <james@jamesteitsworth.com>
2020-08-15 19:05:15 -0500

    Customize the welcome message

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Guess the isogram and win a prize");
+    PrintLine("Input your guess below.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
