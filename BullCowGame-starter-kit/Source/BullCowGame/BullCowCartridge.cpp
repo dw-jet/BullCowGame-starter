c0895ea6b85a61c73c1f15487d6092e76137a7e1
James Teitsworth <james@jamesteitsworth.com>
2020-08-15 19:57:01 -0500

    Make sure word is accessible to all functions and check for win

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,42 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    // Set up game
+    HiddenWord = TEXT("thing");
+    // Set lives
+
+    // Welcome player
     PrintLine(TEXT("Guess the isogram and win a prize"));
-    PrintLine(TEXT("Input your guess below."));
+    PrintLine(TEXT("Guess the 5 letter word")); // TODO remove magic number
+    PrintLine(TEXT("Press enter to continue..."));
+
+    // Prompt Player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("thing");
+
+    // Checking PlayerGuess
+    
+    if (Input == HiddenWord)
+    {
+        PrintLine("I'm out. I don't fuck with psychics.");
+    }
+    else
+    {
+        PrintLine("What an idiot");
+    }
+    
 }
+// Check if isogram
+// Check right number of characters
+
+// Remove life
+
+// Check if lives > 0
+// If yes guess again
+// If no show gameover and hiddenword
+// Prompt to play again
+// Check user input
+// Play again quit
