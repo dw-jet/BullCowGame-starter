7990121cf5121b6aedf3b436e7028ea901dc2b49
James Teitsworth <james@jamesteitsworth.com>
2020-08-16 10:06:06 -0500

    Set up life system so player gets more than one guess

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -12,6 +12,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
     if (bGameOver)
     {
         ClearScreen();
@@ -29,12 +30,11 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
             if (Input.Len() != HiddenWord.Len())
             {
                 // FString::Printf is included in terminal code
-                PrintLine(TEXT("You do know how to count to %i right?"), HiddenWord.Len()); // TODO remove magic number
-                PrintLine(TEXT("Now let's try this again."));
+                PrintLine(TEXT("You do know how to count to %i right?"), HiddenWord.Len());
             }
 
             PrintLine("What an idiot");
-            EndGame();
+            LoseALife();
         }
     }
 }
@@ -53,17 +53,32 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 void UBullCowCartridge::SetUpGame()
 {
     HiddenWord = TEXT("thing");
-    Lives = 3;
+    Lives = HiddenWord.Len();
     bGameOver = false;
     ClearScreen();
     // Welcome player
     PrintLine(TEXT("Guess the isogram and win a prize"));
-    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len()); // TODO remove magic number
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
     PrintLine(TEXT("Type in your guess and press enter"));
+    PrintLine(TEXT("You have %i guesses to get it right"), Lives);
 }
 
 void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
     PrintLine("Press enter to play again");
+}
+
+void UBullCowCartridge::LoseALife()
+{
+    Lives -= 1;
+    if (Lives < 1)
+    {
+        PrintLine(TEXT("You are the weakest link. Goodbye."));
+        EndGame();
+    }
+    else
+    {
+        PrintLine(TEXT("Only %i guesses left"), Lives);
+    }
 }
